88b4c06c6651309575f44ded8f899f51ba5b5975
Matyas <59233911+matyaskrecek97@users.noreply.github.com>
2023-10-10 08:54:29 +0200

    Utils refactor, remove comments

--- utils.py
+++ utils.py
@@ -1,18 +1,3 @@
-# Copyright 2021 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-"""Utility functions to display the pose detection results."""
-
 import cv2
 import numpy as np
 from tflite_support.task import processor
@@ -29,16 +14,6 @@ def visualize(
     detection_result: processor.DetectionResult,
     categories: list,
 ) -> np.ndarray:
-    """Draws bounding boxes on the input image and return it.
-
-    Args:
-      image: The input RGB image.
-      detection_result: The list of all "Detection" entities to be visualize.
-
-    Returns:
-      Image with bounding boxes.
-    """
-
     for detection in detection_result.detections:
         category = detection.categories[0]
 
