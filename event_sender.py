925cc4253e21c30e6234765780675e606d3794c7
matyaskrecek97 <59233911+matyaskrecek97@users.noreply.github.com>
2023-11-04 20:24:23 +0100

    Add headers to EventSender

--- event_sender.py
+++ event_sender.py
@@ -6,13 +6,14 @@ import time
 class EventSender:
     def __init__(self):
         self.location_id = os.environ.get('LOCATION_ID')
-        # self.server_url = os.environ.get('SERVER_URL')
-        self.server_url = "http://192.168.1.108:3000"
+        self.server_url = os.environ.get('SERVER_URL')
+        self.token = os.environ.get('TOKEN')
+        self.headers = {'Authorization': 'Bearer ' + self.token}
 
     def send_detection_event(self):
         url = f'{self.server_url}/detection'
         data = {'locationId': self.location_id}
-        response = requests.post(url, data=data)
+        response = requests.post(url, json=data, headers=self.headers)
         print("Send detection request:", response.status_code,
               response.reason, response.text)
         return response
@@ -20,7 +21,7 @@ class EventSender:
     def send_heartbeat_event(self):
         url = f'{self.server_url}/heartbeat'
         data = {'locationId': self.location_id}
-        response = requests.post(url, data=data)
+        response = requests.post(url, json=data, headers=self.headers)
         print("Send heartbeat request:", response.status_code,
               response.reason, response.text)
         return response
@@ -30,3 +31,8 @@ class EventSender:
         while True:
             self.send_heartbeat_event()
             time.sleep(60)
+
+
+if __name__ == '__main__':
+    sender = EventSender()
+    sender.start_hearbeat()
